51a2853198e5f34fce94bbd63ddfab96d2939b66
ouyangjiangchuan <1366325462@qq.com>
2021-12-12 01:01:19 +0800

    feat: 测试提交

--- .cz-config.js
+++ .cz-config.js
@@ -1,18 +1,18 @@
 'use strict'
 module.exports = {
   types: [
-    { value: ':sparkles: feat', name: '✨ 特性 | 一个新的特性' },
-    { value: ':bug: fix', name: '🐛 修复 | 修复一个Bug' },
-    { value: ':tada: init', name: '🎉 Init | 初始化' },
-    { value: ':memo: docs', name: '📝 文档 | 文档' },
-    { value: ':lipstick: style', name: '💄 样式 | 样式' },
-    { value: ':recycle: refactor', name: '♻️  重构 | 代码重构' },
-    { value: ':zap: perf', name: '⚡️ 性能 | 性能优化' },
-    { value: ':white_check_mark: test', name: '✅ 测试 | 添加测试' },
-    { value: ':rocket: chore', name: '🚀 工具 | 构建/工程依赖/工具' },
-    { value: ':rewind: revert', name: '⏪ 回滚 | 代码回退' },
-    { value: ':package: build', name: '📦‍ 打包 | 打包构建' },
-    { value: ':construction_worker: ci', name: '👷 CI | CI 配置' }
+    { value: 'feat', name: '✨ 特性 | 一个新的特性' }, // :sparkles:
+    { value: 'fix', name: '🐛 修复 | 修复一个Bug' }, // :bug:
+    { value: 'init', name: '🎉 Init | 初始化' }, // :tada:
+    { value: 'docs', name: '📝 文档 | 文档' }, // :memo:
+    { value: 'style', name: '💄 样式 | 样式' }, // :lipstick:
+    { value: 'refactor', name: '♻️  重构 | 代码重构' }, // :recycle:
+    { value: 'perf', name: '⚡️ 性能 | 性能优化' }, // :zap:
+    { value: 'test', name: '✅ 测试 | 添加测试' }, // :white_check_mark:
+    { value: 'chore', name: '🚀 工具 | 构建/工程依赖/工具' }, // :rocket:
+    { value: 'revert', name: '⏪ 回滚 | 代码回退' }, // :rewind:
+    { value: 'build', name: '📦‍ 打包 | 打包构建' }, // :package:
+    { value: 'ci', name: '👷 CI | CI 配置' } // :construction_worker:
   ],
   messages: {
     type: '请选择提交类型(必填)',
--- CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-# Changelog
-
-All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
-
-### 1.1.4 (2021-12-11)
-
-### Features
-
-- chore(release): 1.0.4 ([6f329e8](https://github.com/oyjiangchuan/workflow-demo/commit/6f329e84fb03d21e1e695c57b014f0906be6cff5))
